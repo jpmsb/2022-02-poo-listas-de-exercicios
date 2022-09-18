4cba193801499945c07e80ad53ed4d9a912129c9
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2022-09-17 22:31:43 -0300

    Exercício 03-b concluído

---
+++ lista-01/CampoMinado.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public class CampoMinado {
+    public static void main(String[] args) {
+        int tamanho = 9;
+        int quantidadeDeMinas = 10;
+        String minas = ",";
+
+        Random numeroAleatorio = new Random();
+        for (int i = 0; i < quantidadeDeMinas; i++) {
+            minas += numeroAleatorio.nextInt(80) + ",";
+        }
+
+        int contador = 0;
+        System.out.println(minas);
+
+        // Criando a matriz
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                if (minas.matches(".*," + contador + ",.*")){
+                    System.out.print("* " );
+                } else System.out.print(". ");
+                contador++;
+                
+            }
+            System.out.print("\n");
+        }
+    }
+}
